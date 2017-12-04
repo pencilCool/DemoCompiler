9299438239b2470bd0dc77e597bcb237af8bc8be
tangyuhua [汤煜华] <tangyuhua@xiaomi.com>
2017-12-04 11:05:45 +0800

    init

---
+++ main.m
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+#define DEFINEEight 8
+
+int main(){
+    @autoreleasepool {
+        int eight = DEFINEEight;
+        int six = 6;
+        NSString* site = [[NSString alloc] initWithUTF8String:”starming”];
+        int rank = eight + six;
+        NSLog(@“%@ rank %d”, site, rank);
+    }
+    return 0;
+}
